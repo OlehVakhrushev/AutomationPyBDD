0a0027f9ce1298e66fc68d90ed423285014b918e
Akradabas <akradabaska@gmail.com>
2021-03-04 22:06:48 -0800

    added more tests

--- AutomationClass/Test.feature
+++ AutomationClass/Test.feature
@@ -1,4 +1,12 @@
-Feature: Hello world
+#Feature: Hello world
+#
+# Scenario: Test
+#   Given Navigate to Google
 
- Scenario: Test
-   Given Navigate to Google
+Feature: Regression for Ebay
+
+  Scenario: Verify that search display right items
+    Given Open eBay.com
+    And Search for "dress"
+    And Click the Search button
+    Then All items are somewhat dress related
--- AutomationClass/steps/definitions.py
+++ AutomationClass/steps/definitions.py
@@ -1,8 +1,33 @@
 from behave import step
 from selenium import webdriver
+from time import sleep
 
 
-@step('Navigate to Google')
-def test(context):
-    driver = webdriver.Chrome("C:/Python39/Scripts/chromedriver.exe")
-    driver.get("https://www.google.com/")
+# @step('Navigate to Google')
+# def test(context):
+#     driver = webdriver.Chrome("C:/Python39/Scripts/chromedriver.exe")
+#     driver.get("https://www.google.com/")
+
+
+@step('Open eBay.com')
+def some_test_imp(context):
+    context.browser = webdriver.Chrome()
+    context.browser.get("https://www.ebay.com/")
+
+
+@step('Search for "dress"')
+def search_smth(context):
+    search_inpt = context.browser.find_element_by_xpath("//input[@id='gh-ac']")
+    search_inpt.send_keys("dress")
+
+
+@step('Click the Search button')
+def click_the_search_btn(context):
+    search_btn = context.browser.find_element_by_xpath("//input[@id='gh-btn']")
+    search_btn.click()
+    sleep(5)
+
+
+@step('All items are somewhat dress related')
+def verify_search_result(context):
+    item = context.browser.find_element_by_xpath("//h3[text()='Women Ladies Long Maxi Dress Boho Holiday Beach Party Cocktail Summer Sundress']")
