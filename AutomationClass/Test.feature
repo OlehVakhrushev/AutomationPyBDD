e5c55357b598485e5891b69ebdec5146413d2654
Akradabas <akradabaska@gmail.com>
2021-03-07 21:15:06 -0800

    Added more Scenarios

--- AutomationClass/Test.feature
+++ AutomationClass/Test.feature
@@ -5,26 +5,59 @@
 
 Feature: Regression for Ebay
 
-#  Scenario: Verify that search display right items
-#    Given Open eBay.com
-#    And Search for "dress"
-#    And Click the Search button
+  Scenario: Verify that search display right items
+    Given Open eBay.com
+    And Search for "dress"
+    And Click the Search button
 #    Then All items are somewhat dress related
 
 
-#  Scenario: Verify Top Bar functions
-#    Given Go to shopping cart
-#    Given Go to Sell
-#    Given Go to Help & Contact
-#    Given Go to Brand Outlet
-#    Given Go to Daily Deals
+  Scenario: Verify Top Bar functions
+    Given Go to shopping cart
+    And Go to Sell
+    And Go to Help & Contact
+    And Go to Brand Outlet
+    Then Go to Daily Deals
+
+  Scenario: Find product and show the price
+    Given Open eBay.com
+    Then Search toy product
+    And Click Search
+    Then Show price of first product
+
+  Scenario: Verify Sign In with wrong password and email
+    Given Open eBay.com
+    And Click Sign In
+    And Input email
+    And Click Continue
+    And Input wrong password
+    And Click Sign In button
+    Then Verify fail
+
+  Scenario: Use search with srt and num
+    Given Open eBay.com
+    And Search for "dress123"
+    And Click Search
+    Then Show results
+
+  Scenario: Use search with numbers only
+    Given Open eBay.com
+    And Search for 123
+    And Click Search
+    Then Show results
+
+  Scenario: Use search with num, str, spec characters
+    Given Open eBay.com
+    And Search for dress^#$%123
+    And Click Search
+    Then Show results
+
+  Scenario: Use search with space " "
+    Given Open eBay.com
+    And Search for " "
+    And Click Search
+    Then Show results
 
 
-#  Scenario: Verify Sign In with wrong password and email
-#    Given Open eBay.com
-#    Given Click Sign In
-#    Given Input email
-#    Given Click Continue
-#    Given Input wrong password
-#    Given Click Sign In button
-#    Given Verify fail
+
+
--- AutomationClass/steps/definitions.py
+++ AutomationClass/steps/definitions.py
@@ -3,33 +3,33 @@ from selenium import webdriver
 from time import sleep
 
 
-# @step('Navigate to Google')
-# def test(context):
-#     driver = webdriver.Chrome("C:/Python39/Scripts/chromedriver.exe")   # path for windows #
-#     driver.get("https://www.google.com/")
-#     print("Navigate OK")
-#
-#
-# @step('Open eBay.com')
-# def some_test_imp(context):
-#     context.browser = webdriver.Chrome()
-#     context.browser.get("https://www.ebay.com/")
-#     print("Open eBay.com OK")
-#
-#
-# @step('Search for "dress"')
-# def search_smth(context):
-#     search_inpt = context.browser.find_element_by_xpath("//input[@id='gh-ac']")
-#     search_inpt.send_keys("dress")
-#     print("Search OK")
-#
-#
-# @step('Click the Search button')
-# def click_the_search_btn(context):
-#     search_btn = context.browser.find_element_by_xpath("//input[@id='gh-btn']")
-#     search_btn.click()
-#     sleep(2)
-#     print("Click the Search button OK")
+@step('Navigate to Google')
+def test(context):
+    driver = webdriver.Chrome("C:/Python39/Scripts/chromedriver.exe")   # path for windows #
+    driver.get("https://www.google.com/")
+    print("Navigate OK")
+
+
+@step('Open eBay.com')
+def some_test_imp(context):
+    context.browser = webdriver.Chrome()
+    context.browser.get("https://www.ebay.com/")
+    print("Open eBay.com OK")
+
+
+@step('Search for "dress"')
+def search_smth(context):
+    search_inpt = context.browser.find_element_by_xpath("//input[@id='gh-ac']")
+    search_inpt.send_keys("dress")
+    print("Search OK")
+
+
+@step('Click the Search button')
+def click_the_search_btn(context):
+    search_btn = context.browser.find_element_by_xpath("//input[@id='gh-btn']")
+    search_btn.click()
+    sleep(2)
+    print("Click the Search button OK")
 
 
 # @step('All items are somewhat dress related')
@@ -37,113 +37,298 @@ from time import sleep
 #     item = context.browser.find_element_by_xpath("//h3[text()='Women Ladies Long Maxi Dress Boho Holiday Beach Party Cocktail Summer Sundress']")
 
 
-# @step('Go to shopping cart')
-# def verify_shopping_cart(context):
-#     context.browser = webdriver.Chrome()
-#     context.browser.get("https://www.ebay.com/")
-#     cart = context.browser.find_element_by_xpath("//a[@title='Your shopping cart']")
-#     cart.click()
-#     cart_title = context.browser.find_element_by_xpath("//h1[text()='Shopping cart']").text
-#     assert 'Shopping cart' == cart_title
-#     sleep(2)
-#     print("Cart OK")
-#
-#
-# @step('Go to Sell')
-# def verify_sell(context):
-#     context.browser.get("https://www.ebay.com/")
-#     sell = context.browser.find_element_by_xpath("//li[@id='gh-p-2']")
-#     sell.click()
-#     sell_title = context.browser.find_element_by_xpath("//h1[@class='home-pagetitle']").text
-#     assert 'Selling' == sell_title
-#     sleep(2)
-#     print("Sell OK")
-#
-#
-# @step('Go to Help & Contact')
-# def verify_help(context):
-#     context.browser.get("https://www.ebay.com/")
-#     help = context.browser.find_element_by_xpath("//a[text()=' Help & Contact']")
-#     help.click()
-#     help_title = context.browser.find_element_by_xpath("//button[text()='Help']").text
-#     assert "Help" == help_title
-#     sleep(2)
-#     print("Help OK")
-#
-#
-# @step('Go to Brand Outlet')
-# def verify_brand_outlet(context):
-#     context.browser.get("https://www.ebay.com/")
-#     context.browser.maximize_window()
-#     brand = context.browser.find_element_by_xpath("//a[text()=' Brand Outlet']")
-#     brand.click()
-#     brand_title = context.browser.find_element_by_xpath("//span[text()='The Brand Outlet']").text
-#     assert "The Brand Outlet" == brand_title
-#     sleep(2)
-#     print("Brand OK")
-#
-#
-# @step('Go to Daily Deals')
-# def verify_daily_deals(context):
-#     context.browser.get("https://www.ebay.com/")
-#     deals = context.browser.find_element_by_xpath("//a[text()=' Daily Deals']")
-#     deals.click()
-#     deals_title = context.browser.find_element_by_xpath("//a[text()='Deals']").text
-#     assert "Deals" == deals_title
-#     sleep(2)
-#     print("Deals OK")
+@step('Go to shopping cart')
+def verify_shopping_cart(context):
+    context.browser = webdriver.Chrome()
+    context.browser.get("https://www.ebay.com/")
+    cart = context.browser.find_element_by_xpath("//a[@title='Your shopping cart']")
+    cart.click()
+    cart_title = context.browser.find_element_by_xpath("//h1[text()='Shopping cart']").text
+    assert 'Shopping cart' == cart_title
+    sleep(2)
+    print("Cart OK")
 
 
-# @step('Open eBay.com')
-# def some_test_imp(context):
-#     context.browser = webdriver.Chrome()
-#     context.browser.get("https://www.ebay.com/")
-#     print("Open eBay.com OK")
-#
-#
-# @step('Click Sign In')
-# def sign_in(context):
-#     sing_in = context.browser.find_element_by_xpath("//a[@_sp='m570.l1524' and  text()='Sign in']")
-#     sing_in.click()
-#     sleep(3)
-#     print("Click Sign In")
-#
-#
-# @step('Input email')
-# def email(context):
-#     email_field = context.browser.find_element_by_xpath("//input[@id='userid']")
-#     email_field.send_keys("akradabaska@gmail.com")
-#     sleep(2)
-#     print("Input email OK")
-#
-#
-# @step('Click Continue')
-# def cont(context):
-#     cont = context.browser.find_element_by_xpath("//button[@id='signin-continue-btn']")
-#     cont.click()
-#     sleep(2)
-#     print("Click Continue OK")
-#
-#
-# @step('Input wrong password')
-# def password(context):
-#     pass_field = context.browser.find_element_by_xpath("//input[@id='pass']")
-#     pass_field.send_keys("123123123")
-#     sleep(2)
-#     print("Input wrong password OK")
-#
-#
-# @step('Click Sign In button')
-# def cont_btn_click(context):
-#     cont_btn = context.browser.find_element_by_xpath("//button[@id='sgnBt']")
-#     cont_btn.click()
-#     sleep(2)
-#     print("Click Sign In button OK")
-#
-#
-# @step('Verify fail')
-# def verify_fail(context):
-#     fail = context.browser.find_element_by_xpath("//p[@id='signin-error-msg']").text
-#     assert "Oops, that's not a match." == fail
-#     sleep(2)
-#     print("Verify fail OK")
+@step('Go to Sell')
+def verify_sell(context):
+    context.browser.get("https://www.ebay.com/")
+    sell = context.browser.find_element_by_xpath("//li[@id='gh-p-2']")
+    sell.click()
+    sell_title = context.browser.find_element_by_xpath("//h1[@class='home-pagetitle']").text
+    assert 'Selling' == sell_title
+    sleep(2)
+    print("Sell OK")
+
+
+@step('Go to Help & Contact')
+def verify_help(context):
+    context.browser.get("https://www.ebay.com/")
+    help = context.browser.find_element_by_xpath("//a[text()=' Help & Contact']")
+    help.click()
+    help_title = context.browser.find_element_by_xpath("//button[text()='Help']").text
+    assert "Help" == help_title
+    sleep(2)
+    print("Help OK")
+
+
+@step('Go to Brand Outlet')
+def verify_brand_outlet(context):
+    context.browser.get("https://www.ebay.com/")
+    context.browser.maximize_window()
+    brand = context.browser.find_element_by_xpath("//a[text()=' Brand Outlet']")
+    brand.click()
+    brand_title = context.browser.find_element_by_xpath("//span[text()='The Brand Outlet']").text
+    assert "The Brand Outlet" == brand_title
+    sleep(2)
+    print("Brand OK")
+
+
+@step('Go to Daily Deals')
+def verify_daily_deals(context):
+    context.browser.get("https://www.ebay.com/")
+    deals = context.browser.find_element_by_xpath("//a[text()=' Daily Deals']")
+    deals.click()
+    deals_title = context.browser.find_element_by_xpath("//a[text()='Deals']").text
+    assert "Deals" == deals_title
+    sleep(2)
+    print("Deals OK")
+
+
+@step('Open eBay.com')
+def open_ebay(context):
+    context.browser = webdriver.Chrome()
+    context.browser.get("https://www.ebay.com/")
+    print("Open eBay.com OK")
+
+
+@step('Click Sign In')
+def sign_in(context):
+    sing_in = context.browser.find_element_by_xpath("//a[@_sp='m570.l1524' and  text()='Sign in']")
+    sing_in.click()
+    sleep(3)
+    print("Click Sign In")
+
+
+@step('Input email')
+def email(context):
+    email_field = context.browser.find_element_by_xpath("//input[@id='userid']")
+    email_field.send_keys("akradabaska@gmail.com")
+    sleep(2)
+    print("Input email OK")
+
+
+@step('Click Continue')
+def cont(context):
+    cont = context.browser.find_element_by_xpath("//button[@id='signin-continue-btn']")
+    cont.click()
+    sleep(2)
+    print("Click Continue OK")
+
+
+@step('Input wrong password')
+def password(context):
+    pass_field = context.browser.find_element_by_xpath("//input[@id='pass']")
+    pass_field.send_keys("123123123")
+    sleep(2)
+    print("Input wrong password OK")
+
+
+@step('Click Sign In button')
+def cont_btn_click(context):
+    cont_btn = context.browser.find_element_by_xpath("//button[@id='sgnBt']")
+    cont_btn.click()
+    sleep(2)
+    print("Click Sign In button OK")
+
+
+@step('Verify fail')
+def verify_fail(context):
+    fail = context.browser.find_element_by_xpath("//p[@id='signin-error-msg']").text
+    assert "Oops, that's not a match." == fail
+    sleep(2)
+    print("Verify fail OK")
+
+
+@step('Open eBay.com')
+def open_ebay(context):
+    context.browser = webdriver.Chrome()
+    context.browser.get("https://www.ebay.com/")
+    print("Open eBay.com OK")
+
+
+@step('Search {search_product} product')
+def search_toy(context, search_product):
+    search = context.browser.find_element_by_xpath("//input[@id='gh-ac']")
+    search.send_keys("{}".format(search_product))
+    sleep(2)
+    print("Search product OK")
+
+
+@step('Click Search')
+def click_search(context):
+    search_btn = context.browser.find_element_by_xpath("//input[@id='gh-btn']")
+    search_btn.click()
+    sleep(2)
+    print("Click Search OK")
+
+
+@step('Show price of first product')
+def show_price(context):
+    price = context.browser.find_element_by_xpath("//span[@class='s-item__price']").text
+    sleep(3)
+    print(f"Show price of first product OK and {price}")
+
+
+@step('Open eBay.com')
+def open_ebay(context):
+    context.browser = webdriver.Chrome()
+    context.browser.get("https://www.ebay.com/")
+    print("Open eBay.com OK")
+
+
+@step('Search for "dress123"')
+def search_smth(context):
+    search_inpt = context.browser.find_element_by_xpath("//input[@id='gh-ac']")
+    search_inpt.send_keys("dress123")
+    sleep(2)
+    print("Search OK")
+
+
+@step('Click Search')
+def click_search(context):
+    search_btn = context.browser.find_element_by_xpath("//input[@id='gh-btn']")
+    search_btn.click()
+    sleep(2)
+    print("Click Search OK")
+
+
+@step('Show results')
+def show_res(context):
+    res = context.browser.find_element_by_xpath("//h1[@class='srp-controls__count-heading']").text
+    print(f"{res}  <=== Here is your results")
+    sleep(2)
+    print("Show results OK")
+
+
+@step('Open eBay.com')
+def open_ebay(context):
+    context.browser = webdriver.Chrome()
+    context.browser.get("https://www.ebay.com/")
+    print("Open eBay.com OK")
+
+
+@step('Search for "dress123%^&&"')
+def search_smth(context):
+    search_inpt = context.browser.find_element_by_xpath("//input[@id='gh-ac']")
+    search_inpt.send_keys("dress123%^&&")
+    sleep(2)
+    print("Search OK")
+
+
+@step('Click Search')
+def click_search(context):
+    search_btn = context.browser.find_element_by_xpath("//input[@id='gh-btn']")
+    search_btn.click()
+    sleep(2)
+    print("Click Search OK")
+
+
+@step('Show results')
+def show_res(context):
+    res = context.browser.find_element_by_xpath("//h1[@class='srp-controls__count-heading']").text
+    print(f"{res}  <=== Here is your results")
+    sleep(2)
+    print("Show results OK")
+
+
+@step('Open eBay.com')
+def open_ebay(context):
+    context.browser = webdriver.Chrome()
+    context.browser.get("https://www.ebay.com/")
+    print("Open eBay.com OK")
+
+
+@step('Search for 123')
+def search_smth(context):
+    search_inpt = context.browser.find_element_by_xpath("//input[@id='gh-ac']")
+    search_inpt.send_keys("123")
+    sleep(2)
+    print("Search OK")
+
+
+@step('Click Search')
+def click_search(context):
+    search_btn = context.browser.find_element_by_xpath("//input[@id='gh-btn']")
+    search_btn.click()
+    sleep(2)
+    print("Click Search OK")
+
+
+@step('Show results')
+def show_res(context):
+    res = context.browser.find_element_by_xpath("//h1[@class='srp-controls__count-heading']").text
+    print(f"{res}  <=== Here is your results")
+    sleep(2)
+    print("Show results OK")
+
+
+@step('Open eBay.com')
+def open_ebay(context):
+    context.browser = webdriver.Chrome()
+    context.browser.get("https://www.ebay.com/")
+    print("Open eBay.com OK")
+
+
+@step('Search for dress^#$%123')
+def search_smth(context):
+    search_inpt = context.browser.find_element_by_xpath("//input[@id='gh-ac']")
+    search_inpt.send_keys("dress^#$%123")
+    sleep(2)
+    print("Search OK")
+
+
+@step('Search for dress^#$%123')
+def click_search(context):
+    search_btn = context.browser.find_element_by_xpath("//input[@id='gh-btn']")
+    search_btn.click()
+    sleep(2)
+    print("Click Search OK")
+
+
+@step('Show results')
+def show_res(context):
+    res = context.browser.find_element_by_xpath("//h1[@class='srp-controls__count-heading']").text
+    print(f"{res}  <=== Here is your results")
+    sleep(2)
+    print("Show results OK")
+
+
+@step('Open eBay.com')
+def open_ebay(context):
+    context.browser = webdriver.Chrome()
+    context.browser.get("https://www.ebay.com/")
+    print("Open eBay.com OK")
+
+
+@step('Search for " "')
+def search_smth(context):
+    search_inpt = context.browser.find_element_by_xpath("//input[@id='gh-ac']")
+    search_inpt.send_keys(" ")
+    sleep(2)
+    print("Search OK")
+
+
+@step('Click Search')
+def click_search(context):
+    search_btn = context.browser.find_element_by_xpath("//input[@id='gh-btn']")
+    search_btn.click()
+    sleep(2)
+    print("Click Search OK")
+
+
+@step('Show results')
+def show_res(context):
+    res_error = context.browser.find_element_by_xpath("//div[@class='s-message__content']").text
+    print(f"{res_error}  <=== Here is your error result with space search")
+    sleep(2)
+    print("Show results OK")
